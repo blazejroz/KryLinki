a5958846d42b4f3ed60a159a0a2289c03a045155
blazejroz <44708701+blazejroz@users.noreply.github.com>
2019-02-16 10:12:32 +0100

    m

--- wp-login.php
+++ wp-login.php
@@ -9,11 +9,11 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 	<title>Zaloguj się &lsaquo; 4 WDH KryLinki &#8212; WordPress</title>
 	<link rel='dns-prefetch' href='//s.w.org' />
-<!--n2css--><link rel='stylesheet' id='dashicons-css'  href='/https://blazejroz.github.io/Krylinki/wp-includes/css/dashicons.min.css?ver=5.0.3' type='text/css' media='all' />
-<link rel='stylesheet' id='buttons-css'  href='/https://blazejroz.github.io/Krylinki/wp-includes/css/buttons.min.css?ver=5.0.3' type='text/css' media='all' />
-<link rel='stylesheet' id='forms-css'  href='/https://blazejroz.github.io/Krylinki/wp-admin/css/forms.min.css?ver=5.0.3' type='text/css' media='all' />
-<link rel='stylesheet' id='l10n-css'  href='/https://blazejroz.github.io/Krylinki/wp-admin/css/l10n.min.css?ver=5.0.3' type='text/css' media='all' />
-<link rel='stylesheet' id='login-css'  href='/https://blazejroz.github.io/Krylinki/wp-admin/css/login.min.css?ver=5.0.3' type='text/css' media='all' />
+<!--n2css--><link rel='stylesheet' id='dashicons-css'  href='./wp-includes/css/dashicons.min.css?ver=5.0.3' type='text/css' media='all' />
+<link rel='stylesheet' id='buttons-css'  href='./wp-includes/css/buttons.min.css?ver=5.0.3' type='text/css' media='all' />
+<link rel='stylesheet' id='forms-css'  href='./wp-admin/css/forms.min.css?ver=5.0.3' type='text/css' media='all' />
+<link rel='stylesheet' id='l10n-css'  href='./wp-admin/css/l10n.min.css?ver=5.0.3' type='text/css' media='all' />
+<link rel='stylesheet' id='login-css'  href='./wp-admin/css/login.min.css?ver=5.0.3' type='text/css' media='all' />
 	<meta name='robots' content='noindex,noarchive' />
 	<meta name='referrer' content='strict-origin-when-cross-origin' />
 		<meta name="viewport" content="width=device-width" />
@@ -22,7 +22,7 @@
 		<div id="login">
 		<h1><a href="https://pl.wordpress.org/" title="Oparte na WordPressie" tabindex="-1">Oparte na WordPressie</a></h1>
 	
-<form name="loginform" id="loginform" action="/https://blazejroz.github.io/Krylinki/wp-login.php" method="post">
+<form name="loginform" id="loginform" action="./" method="post">
 	<p>
 		<label for="user_login">Nazwa użytkownika lub e-mail<br />
 		<input type="text" name="log" id="user_login" class="input" value="" size="20" /></label>
@@ -34,13 +34,13 @@
 		<p class="forgetmenot"><label for="rememberme"><input name="rememberme" type="checkbox" id="rememberme" value="forever"  /> Zapamiętaj mnie</label></p>
 	<p class="submit">
 		<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="Zaloguj się" />
-		<input type="hidden" name="redirect_to" value="/wp-admin/" />
+		<input type="hidden" name="redirect_to" value="./wp-admin/" />
 		<input type="hidden" name="testcookie" value="1" />
 	</p>
 </form>
 
 <p id="nav">
-	<a href="/https://blazejroz.github.io/Krylinki/wp-login.php?action=lostpassword">Nie pamiętasz hasła?</a>
+	<a href="./?action=lostpassword">Nie pamiętasz hasła?</a>
 </p>
 
 <script type="text/javascript">
@@ -57,7 +57,7 @@ wp_attempt_focus();
 if(typeof wpOnload=='function')wpOnload();
 </script>
 
-	<p id="backtoblog"><a href="/https://blazejroz.github.io/Krylinki/">&larr; Powrót do 4 WDH KryLinki</a></p>
+	<p id="backtoblog"><a href="./index.html">&larr; Powrót do 4 WDH KryLinki</a></p>
 		
 	</div>
 
